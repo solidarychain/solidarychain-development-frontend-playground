923ad5fdd009ce1a2a512dc06259fe4cd4a43f6d
Mario Monteiro <marioammonteiro@gmail.com>
2021-07-09 21:43:03 +0100

    first commit

---
+++ build/precache-manifest.6beedec7eab51f2fc90572f53929ec30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e689013b4be3e937cd69c059d1b0475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a6786c5bd4113d767a",
+    "url": "/static/css/main.146c8ed4.chunk.css"
+  },
+  {
+    "revision": "b6605c493298746ff458",
+    "url": "/static/js/2.c39405dc.chunk.js"
+  },
+  {
+    "revision": "f0f192888aadeb4637e90791c8ef08c6",
+    "url": "/static/js/2.c39405dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a6786c5bd4113d767a",
+    "url": "/static/js/main.d6a48026.chunk.js"
+  },
+  {
+    "revision": "2fd4794658f4fb4635bd",
+    "url": "/static/js/runtime-main.6bb4ce91.js"
+  },
+  {
+    "revision": "b82a644a31d0cd6fe58487f4ae7f7dac",
+    "url": "/static/media/LogoIcon.b82a644a.svg"
+  }
+]);
